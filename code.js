c74863c0c6b2e8b4249adc1135fce7d629c5402f
Minh Le <leducminh130208@gmail.com>
2025-07-24 17:05:28 +0700

    It's even more random
    - Moved to pseudo-random generation from Fischer-Yates algorithm
    - Changed font, increased name box size to increase loading time (?)

--- code.js
+++ code.js
@@ -2,14 +2,17 @@ const birthday = new Date("2008-12-19");
 const now = new Date(); 
 var rng = new RNG(now.getTime() + birthday.getTime());
 const submit = document.getElementById("submit");
+const edit = document.getElementById("edit");
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
-async function load () { // We need to wrap the loop into an async function for this to work
+async function load () {
   for (var i = 1; i <= 4; i++) {
     $("#spin"+(i)).hide();
     $("#S"+(i)).fadeIn();
-    await timer(936); // then the created Promise can be awaited
+    await timer(936);
   }
+  submit.disabled = false;
+  edit.disabled = false;
 }
 submit.onclick = function() 
 {
@@ -21,20 +24,33 @@ submit.onclick = function()
     $("#spin2").fadeIn();
     $("#spin3").fadeIn();
     $("#spin4").fadeIn();
-    let array = [1, 2, 3, 4];
-    for (let i = 3; i > 1; i--) {
-        let j = rng.random(0, i);
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    let i = rng.random(0, 3)
-    if (i == 3)
+    submit.disabled = true;
+    edit.disabled = true;
+
+    const order = new Set([]);
+    while(order.size < 3)
     {
-        let j = rng.random(0, 3);
-        [array[0], array[j]] = [array[j], array[0]];
+      let n = rng.random(1,936);
+      if (n%4==0)
+      {
+        n = 4;
+      }
+      else
+      {
+        n = n%4;
+      }
+      //console.log(n);
+      order.add(n);
     }
+    for(let i = 1; i<=4; i++)
+    {
+      order.add(i);
+    }
+    let arr = Array.from(order);
     for (let i = 0; i < 4; i++)
     {
-        let n = array[i];
+        let n = arr[i];
+        //console.log("S"+arr[i]);
         document.getElementById("S"+(i+1)).innerHTML=document.getElementById("nameS"+n).value;
     }
     setTimeout(function(){ 
