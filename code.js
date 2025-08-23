76c494ff4957a4b94611d628a6b32efb7d7a0481
Minh Le <leducminh130208@gmail.com>
2025-08-23 22:57:04 +0700

    Based on a real-life scenario
    - Added 5 player mode

--- code.js
+++ code.js
@@ -3,10 +3,31 @@ const now = new Date();
 var rng = new RNG(now.getTime() + birthday.getTime());
 const submit = document.getElementById("submit");
 const edit = document.getElementById("edit");
-const timer = ms => new Promise(res => setTimeout(res, ms))
+const timer = ms => new Promise(res => setTimeout(res, ms));
+var count;
+const b4 = document.getElementById("btn4p");
 
-async function load () {
-  for (var i = 1; i <= 4; i++) {
+if (document.querySelector('input[name="select"]')) {
+  document.querySelectorAll('input[name="select"]').forEach((elem) => {
+    elem.addEventListener("change", function(live) {
+      count = live.target.value;
+      //console.log(count);
+      if (count == 4)
+      {
+        $("#c5").hide();
+        $("#nameS5").hide();
+      }
+      else
+      {
+        $("#c5").show();
+        $("#nameS5").show();
+      }
+    });
+  });
+}
+
+async function load (count) {
+  for (var i = 1; i<=count; i++) {
     $("#spin"+(i)).hide();
     $("#S"+(i)).fadeIn();
     await timer(936);
@@ -14,8 +35,22 @@ async function load () {
   submit.disabled = false;
   edit.disabled = false;
 }
+
 submit.onclick = function() 
 {
+    if (b4.checked == true)
+    {
+      count = 4;
+    }
+    else
+    {
+      count = 5;
+    }
+    //console.log(count);
+    for (let i = 0; i < 5; i++)
+    {
+        document.getElementById("S"+(i+1)).innerHTML=' ';
+    }
     $("#S1").hide();
     $("#S2").hide();
     $("#S3").hide();
@@ -24,36 +59,41 @@ submit.onclick = function()
     $("#spin2").fadeIn();
     $("#spin3").fadeIn();
     $("#spin4").fadeIn();
+    if(count == 5)
+    {
+      $("#S5").hide();
+      $("#spin5").fadeIn();
+    }
     submit.disabled = true;
     edit.disabled = true;
 
     const order = new Set([]);
-    while(order.size < 3)
+    while(order.size < count-1)
     {
-      let n = rng.random(1,936);
-      if (n%4==0)
+      let n = rng.random(1,360);
+      if (n%count==0)
       {
-        n = 4;
+        n = count;
       }
       else
       {
-        n = n%4;
+        n = n%count;
       }
       //console.log(n);
       order.add(n);
     }
-    for(let i = 1; i<=4; i++)
+    for(let i = 1; i<=count; i++)
     {
       order.add(i);
     }
     let arr = Array.from(order);
-    for (let i = 0; i < 4; i++)
+    for (let i = 0; i < count; i++)
     {
         let n = arr[i];
         //console.log("S"+arr[i]);
         document.getElementById("S"+(i+1)).innerHTML=document.getElementById("nameS"+n).value;
     }
     setTimeout(function(){ 
-      load();
+      load(count);
     }, rng.random(1500,3000));
 }
